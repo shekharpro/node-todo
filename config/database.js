a76009d050932da6e0c63f950025df1d4da75977
Samaj Shekhar <samajs@thoughtworks.com>
2017-04-22 12:42:15 +0530

    Updted database config to pull from environment variable

--- config/database.js
+++ config/database.js
@@ -1,4 +1,5 @@
+var DB_SERVER_URL = process.env.DB_SERVER_URL
 module.exports = {
     remoteUrl : 'mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu',
-    localUrl: 'mongodb://localhost/meanstacktutorials'
+    localUrl: DB_SERVER_URL
 };
